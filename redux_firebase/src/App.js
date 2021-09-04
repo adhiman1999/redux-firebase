a510093bf144ec5fa119fb8dc9d6900b07060936
phantomkid <adhiman1999@gmail.com>
2021-09-05 03:38:10 +0530

    adding nav links

---
+++ redux_firebase/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Navbar />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
